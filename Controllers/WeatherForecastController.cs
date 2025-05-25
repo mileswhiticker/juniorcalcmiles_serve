d334214e0d4afa76177468f8c62f8c3db37f320d
Miles Whiticker <miles.whiticker@gmail.com>
2025-05-25 12:27:53 +1000

    chore: only 1 forecast returned instead of 5
    Signed-off-by: Miles Whiticker <miles.whiticker@gmail.com>

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace juniorcalcmiles_serve.Controllers;
 
@@ -21,10 +23,10 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        return Enumerable.Range(1, 1).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
+            TemperatureC = Random.Shared.Next(5, 40),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
         .ToArray();
