77064c60a34726e57df2956f1ba71945f6199e03
Miles Whiticker <miles.whiticker@gmail.com>
2025-05-25 18:42:54 +1000

    feat: response now supports four operations: add, subtract, multiply, divide
    Signed-off-by: Miles Whiticker <miles.whiticker@gmail.com>

--- Operation.cs
+++ Operation.cs
@@ -4,14 +4,16 @@ namespace juniorcalcmiles_serve
     public class OperationRequest
     {
         public int request_id { get; set; }
-        //public string operation_id { get; set; }
-        //public int[] values { get; set; }
+        public string operation_id { get; set; }
+        public int[] values { get; set; }
     }
 
     public class OperationResponse
     {
         private static int nextResponseId = 0;
-        public string resultString { get; set; }
+        public float resultVal { get; set; }
+        public string responseMessage { get; set; }
+        public bool operationSuccess { get; set; }
         public int requestId { get; set; }
         public int responseId { get; set; }
         public DateTime responseTime { get; set; }
@@ -21,8 +23,85 @@ namespace juniorcalcmiles_serve
             this.responseId = nextResponseId++;
             this.responseTime = DateTime.Now;
 
-            Random random = new Random();
-            this.resultString = "result" + random.Next();
+            float result = 0;
+            switch (request.operation_id)
+            {
+                case "add":
+                    {
+                        for (int i = 0; i < request.values.Length; i++)
+                        {
+                            result += request.values[i];
+                        }
+                        this.operationSuccess = true;
+                        break;
+                    }
+                case "subtract":
+                    {
+                        for (int i = 0; i < request.values.Length; i++)
+                        {
+                            if(i==0)
+                            {
+                                result = request.values[i];
+                            }
+                            else
+                            {
+                                result -= request.values[i];
+                            }
+                        }
+                        this.operationSuccess = true;
+                        break;
+                    }
+                case "multiply":
+                    {
+                        for (int i = 0; i < request.values.Length; i++)
+                        {
+                            if (result == 0)
+                            {
+                                result = request.values[i];
+                            }
+                            else
+                            {
+                                result *= request.values[i];
+                            }
+                        }
+                        this.operationSuccess = true;
+                        break;
+                    }
+                case "divide":
+                    {
+                        for (int i = 0; i < request.values.Length; i++)
+                        {
+                            //limit this to the first two values
+                            if(i > 1)
+                            {
+                                this.responseMessage = "Can only divide exactly two values";
+                                this.operationSuccess = false;
+                                break;
+                            }
+                            if (result == 0)
+                            {
+                                result = request.values[i];
+                            }
+                            else
+                            {
+                                result /= request.values[i];
+                            }
+                        }
+                        this.operationSuccess = true;
+                        break;
+                    }
+                default:
+                    {
+                        this.operationSuccess = false;
+                        this.responseMessage = "Unknown operation: " + request.operation_id;
+                        break;
+                    }
+            }
+            if(this.operationSuccess)
+            { 
+                this.resultVal = result;
+                this.responseMessage = "Operation successful.";
+            }
         }
     }
 }
